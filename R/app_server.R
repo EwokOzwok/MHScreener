d9be561bcbcff8e9a823d8fb8692891118c3479e
EwokOzwok <eeozmat@buffalo.edu>
2023-09-01 18:17:03 -0400

    Initial commit

---
+++ R/app_server.R
@@ -0,0 +1,192 @@
+#' The application server-side
+#'
+#' @param input,output,session Internal parameters for {shiny}.
+#'     DO NOT REMOVE.
+#' @import shiny
+#' @import shinyMobile
+#' @import dplyr
+#' @import shinyalert
+#' @noRd
+app_server <- function(input, output, session) {
+  # Your application server logic
+  ScreenerData<-data.frame(matrix(NA, nrow=1,ncol = 16))
+  colnames(ScreenerData)<-c("PHQ1","PHQ2","PHQ3","PHQ4","PHQ5","PHQ6","PHQ7","PHQ8","PHQ9","GAD1","GAD2","GAD3","GAD4","GAD5","GAD6","GAD7")
+
+
+  observeEvent(input$Start, {
+    updateF7Tabs(session = session, id = "tabs", selected = "GettingStartedTab")
+  })
+
+  observeEvent(input$Screenerprompt, {
+    updateF7Tabs(session = session, id = "tabs", selected = "PHQ9tab")
+  })
+
+
+  observeEvent(input$StartOver, {
+    updateF7Tabs(session = session, id = "tabs", selected = "WelcomeTab")
+    updateF7Select(inputId="PHQitem1", selected = c("Choose a response"))
+    updateF7Select(inputId="PHQitem2", selected = c("Choose a response"))
+    updateF7Select(inputId="PHQitem3", selected = c("Choose a response"))
+    updateF7Select(inputId="PHQitem4", selected = c("Choose a response"))
+    updateF7Select(inputId="PHQitem5", selected = c("Choose a response"))
+    updateF7Select(inputId="PHQitem6", selected = c("Choose a response"))
+    updateF7Select(inputId="PHQitem7", selected = c("Choose a response"))
+    updateF7Select(inputId="PHQitem8", selected = c("Choose a response"))
+    updateF7Select(inputId="PHQitem9", selected = c("Choose a response"))
+    output$datasummaryIntro<- renderUI({})
+
+
+  })
+
+
+
+
+
+
+
+  observeEvent(input$PHQ9Submit, {
+
+    if(input$PHQitem1=="Choose a response" ||
+       input$PHQitem2=="Choose a response" ||
+       input$PHQitem3=="Choose a response" ||
+       input$PHQitem4=="Choose a response" ||
+       input$PHQitem5=="Choose a response" ||
+       input$PHQitem6=="Choose a response" ||
+       input$PHQitem7=="Choose a response" ||
+       input$PHQitem8=="Choose a response" ||
+       input$PHQitem9=="Choose a response")
+    {
+      shinyalert::shinyalert(title="Oops, you forgot to answer a question!",type="error")
+    } else {
+      rm(ScreenerData)
+      ScreenerData<-data.frame(matrix(NA, nrow=1,ncol = 16))
+      colnames(ScreenerData)<-c("PHQ1","PHQ2","PHQ3","PHQ4","PHQ5","PHQ6","PHQ7","PHQ8","PHQ9","GAD1","GAD2","GAD3","GAD4","GAD5","GAD6","GAD7")
+
+      ScreenerData$PHQ1<-input$PHQitem1
+      ScreenerData$PHQ2<-input$PHQitem2
+      ScreenerData$PHQ3<-input$PHQitem3
+      ScreenerData$PHQ4<-input$PHQitem4
+      ScreenerData$PHQ5<-input$PHQitem5
+      ScreenerData$PHQ6<-input$PHQitem6
+      ScreenerData$PHQ7<-input$PHQitem7
+      ScreenerData$PHQ8<-input$PHQitem8
+      ScreenerData$PHQ9<-input$PHQitem9
+      ScreenerData[] <- factor(unlist(ScreenerData), levels = c("Not at all","A little bit","Moderately","Quite a bit","Extremely"), labels = c(1, 2, 3,4,5))
+      ScreenerData<-lapply(ScreenerData,as.numeric)
+      ScreenerData<-as.data.frame(ScreenerData)
+      ScreenerData$Total_PHQ9<-ScreenerData$PHQ1+ScreenerData$PHQ2+ScreenerData$PHQ3+ScreenerData$PHQ4+ScreenerData$PHQ5+ScreenerData$PHQ6+ScreenerData$PHQ7+ScreenerData$PHQ8+ScreenerData$PHQ9
+      print(ScreenerData)
+      updateF7Tabs(session = session, id = "tabs", selected = "GAD7tab")
+
+    }
+
+  })
+
+
+
+
+      output$datasummaryIntro<- renderUI({
+        tagList(
+          f7Shadow(
+            intensity = 5,
+            hover = TRUE,
+            f7Card(
+              title = "",
+              f7Align(h2("Your Mental Health Screener has been successfully completed"), side=c("center")),
+              f7Align(h3("Please hand the tablet back to the Navigator"), side=c("center")),
+              hairlines = F, strong = T, inset =
+                F, tablet = FALSE)
+          ),
+          f7Card(
+            f7Align(h3("Total PHQ9 score"), side=c("center")),
+            f7Align(h3(c(ScreenerData$Total_PHQ9)), side=c("center")),
+            footer = NULL,
+            hairlines = F, strong = T, inset = F, tablet = FALSE)
+        )
+      })
+
+
+      print(ScreenerData$Total_PHQ9)
+
+
+
+
+  output$StartButton<-renderUI({
+    tagList(
+      f7Button("Start", "Get Started!")
+    )
+  })
+
+  output$PHQ9<-renderUI({
+    tagList(
+      f7Card(
+        h4("PHQ Item 1"),
+        f7Select("PHQitem1", NULL , choices = c("Choose a response", "Not at all","A little bit","Moderately","Quite a bit","Extremely"), selected = NULL),
+        footer = NULL,
+        hairlines = F, strong = T, inset = F, tablet = FALSE),
+
+
+      f7Card(
+        h4("PHQ Item 2"),
+        f7Select("PHQitem2", NULL , choices = c("Choose a response", "Not at all","A little bit","Moderately","Quite a bit","Extremely"), selected = NULL),
+        footer = NULL,
+        hairlines = F, strong = T, inset = F, tablet = FALSE),
+
+
+      f7Card(
+        h4("PHQ Item 3"),
+        f7Select("PHQitem3", NULL , choices = c("Choose a response", "Not at all","A little bit","Moderately","Quite a bit","Extremely"), selected = NULL),
+        footer = NULL,
+        hairlines = F, strong = T, inset = F, tablet = FALSE),
+
+
+      f7Card(
+        h4("PHQ Item 4"),
+        f7Select("PHQitem4", NULL , choices = c("Choose a response", "Not at all","A little bit","Moderately","Quite a bit","Extremely"), selected = NULL),
+        footer = NULL,
+        hairlines = F, strong = T, inset = F, tablet = FALSE),
+
+
+      f7Card(
+        h4("PHQ Item 5"),
+        f7Select("PHQitem5", NULL , choices = c("Choose a response", "Not at all","A little bit","Moderately","Quite a bit","Extremely"), selected = NULL),
+        footer = NULL,
+        hairlines = F, strong = T, inset = F, tablet = FALSE),
+
+
+      f7Card(
+        h4("PHQ Item 6"),
+        f7Select("PHQitem6", NULL , choices = c("Choose a response", "Not at all","A little bit","Moderately","Quite a bit","Extremely"), selected = NULL),
+        footer = NULL,
+        hairlines = F, strong = T, inset = F, tablet = FALSE),
+
+
+      f7Card(
+        h4("PHQ Item 7"),
+        f7Select("PHQitem7", NULL , choices = c("Choose a response", "Not at all","A little bit","Moderately","Quite a bit","Extremely"), selected = NULL),
+        footer = NULL,
+        hairlines = F, strong = T, inset = F, tablet = FALSE),
+
+
+      f7Card(
+        h4("PHQ Item 8"),
+        f7Select("PHQitem8", NULL , choices = c("Choose a response", "Not at all","A little bit","Moderately","Quite a bit","Extremely"), selected = NULL),
+        footer = NULL,
+        hairlines = F, strong = T, inset = F, tablet = FALSE),
+
+
+      f7Card(
+        h4("PHQ Item 9"),
+        f7Select("PHQitem9", NULL , choices = c("Choose a response", "Not at all","A little bit","Moderately","Quite a bit","Extremely"), selected = NULL),
+        footer = NULL,
+        hairlines = F, strong = T, inset = F, tablet = FALSE),
+
+
+      f7Button("PHQ9Submit", "Continue")
+    )
+  })
+
+
+
+
+}
---
+++ R/app_ui.R
@@ -0,0 +1,173 @@
+#' The application User-Interface
+#'
+#' @param request Internal parameter for `{shiny}`.
+#'     DO NOT REMOVE.
+#' @import shiny
+#' @import shinyMobile
+#' @import htmltools
+#' @noRd
+app_ui <- function(request) {
+  tagList(
+    # Leave this function for adding external resources
+    golem_add_external_resources(),
+    # Your application UI logic
+
+    shinyMobile::f7Page(
+      title = "Project ACCESS Mental Health Screener",
+      options = list(theme=c("auto"), dark=TRUE, preloader = F,  pullToRefresh=F),
+      allowPWA=TRUE,
+      f7TabLayout(
+        # panels are not mandatory. These are similar to sidebars
+        navbar = f7Navbar(
+          title= "Mental Health Screener"),
+        # f7Tabs is a special toolbar with included navigation
+        f7Tabs(
+          animated = TRUE,
+          id = "tabs",
+          f7Tab(
+            tabName = "WelcomeTab",
+            icon = f7Icon("house_fill"),
+            active = TRUE,
+            hidden= T,
+            f7Block(
+              f7Shadow(
+                intensity = 5,
+                hover = TRUE,
+                f7Card(
+                  f7Align(h2("Welcome to the Project ACCESS Mental Health Screener"),side=c("center")),
+                  uiOutput("StartButton"),
+                  footer = NULL,
+                  hairlines = F, strong = T, inset = F, tablet = FALSE)
+              )
+            )
+          ),
+
+
+          f7Tab(
+            tabName = "GettingStartedTab",
+            icon = f7Icon("house_fill"),
+            active = F,
+            hidden=T,
+            f7Block(
+              f7Shadow(
+                intensity = 5,
+                hover = TRUE,
+                f7Card(
+                  f7Align(h2("To start the screener click the button below"),side=c("center")),
+                  f7Button("Screenerprompt", "Start!"),
+                  footer = NULL,
+                  hairlines = F, strong = T, inset = F, tablet = FALSE)
+              )
+            )
+
+          ),
+
+
+          f7Tab(
+            tabName = "PHQ9tab",
+            icon = f7Icon("search"),
+            active = F,
+            hidden=T,
+            f7Block(
+              f7Shadow(
+                intensity = 5,
+                hover = TRUE,
+                f7Card(
+                  f7Align(h2("Part 1 of 2"), side=c("center")),
+                  h3("INSTRUCTIONS:"),
+                  h4("PHQ9 INSTRUCTIONS HERE!"),
+                  uiOutput("PHQ9"),
+                  footer = NULL,
+                  hairlines = F, strong = T, inset = F, tablet = FALSE)
+              )
+            )
+
+          ),
+
+
+          f7Tab(
+            tabName = "GAD7tab",
+            icon = f7Icon("search"),
+            active = F,
+            hidden=T,
+            f7Block(
+              f7Shadow(
+                intensity = 5,
+                hover = TRUE,
+                f7Card(
+                  f7Align(h2("Part 1 of 2"), side=c("center")),
+                  h3("INSTRUCTIONS:"),
+                  h4("GAD7 INSTRUCTIONS HERE!"),
+                  uiOutput("GAD7"),
+                  footer = NULL,
+                  hairlines = F, strong = T, inset = F, tablet = FALSE)
+              )
+            )
+
+          ),
+
+
+
+
+
+
+
+          f7Tab(
+            tabName = "AppTab1",
+            icon = f7Icon("house_fill"),
+            active = F,
+
+            uiOutput("datasummaryIntro"),
+            f7Shadow(
+              intensity = 5,
+              hover = TRUE,
+              f7Card(
+                title = "",
+                f7Button("StartOver", "Start Over"),
+                hairlines = F, strong = T, inset =
+                  F, tablet = FALSE)
+            )
+          )
+
+
+
+
+
+
+        )
+      )
+    )
+
+
+
+
+
+
+
+  )
+}
+
+#' Add external Resources to the Application
+#'
+#' This function is internally used to add external
+#' resources inside the Shiny application.
+#'
+#' @import shiny
+#' @importFrom golem add_resource_path activate_js favicon bundle_resources
+#' @noRd
+golem_add_external_resources <- function() {
+  add_resource_path(
+    "www",
+    app_sys("app/www")
+  )
+
+  tags$head(
+    favicon(),
+    bundle_resources(
+      path = app_sys("app/www"),
+      app_title = "MHScreener"
+    )
+    # Add here other external resources
+    # for example, you can add shinyalert::useShinyalert()
+  )
+}
