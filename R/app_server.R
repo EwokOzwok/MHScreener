f4ac88a063e470eff7d46e56c6796cc647ae88a7
EwokOzwok <eeozmat@buffalo.edu>
2023-09-11 17:04:55 -0400

    updates

--- R/app_server.R
+++ R/app_server.R
@@ -204,7 +204,7 @@ app_server <- function(input, output, session) {
       } else { output$ResultsInstructions0<- renderUI({})}
 
 
-      if(PHQ9Data$PHQ9 == 1){
+      if(PHQ9Data$Total_PHQ9 > 19 && PHQ9Data$PHQ9 == 1){
         output$ResultsInstructions1<- renderUI({
           tagList(
             hr(),
