2da57c21aa584b54c60ee99f53a10c71d7e65974
EwokOzwok <eeozmat@buffalo.edu>
2023-09-11 00:06:11 -0400

    updated tag

--- R/app_ui.R
+++ R/app_ui.R
@@ -159,11 +159,15 @@ HTML('<script async src="https://www.googletagmanager.com/gtag/js?id=G-5EFHNS6HY
   gtag("config", "G-5EFHNS6HYV");
   </script>'),
 # Google Tag
-HTML('<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({“gtm.start”:
-    new Date().getTime(),event:”gtm.js”});var f=d.getElementsByTagName(s)[0],
-    j=d.createElement(s),dl=l!=“dataLayer”?”&l=“+l:”“;j.async=true;j.src=
-      “https://www.googletagmanager.com/gtm.js?id=“+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,”script”,”dataLayer”,”GTM-KGWTF6SX”);</script>'),
+
+
+
+HTML('<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({"gtm.start":
+      new Date().getTime(),event:"gtm.js"});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!="dataLayer"?"&l="+l:"";j.async=true;j.src=
+        "https://www.googletagmanager.com/gtm.js?id="+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,"script","dataLayer","GTM-KGWTF6SX");</script>'),
+
 
 
 HTML('<link rel="stylesheet" type="text/css" href="https://ewokozwok.github.io/MHScreener/www/framework7.bundle.min.css">')
