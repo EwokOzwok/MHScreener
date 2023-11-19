517e00abcbd87b86142c4d38b1e5c81dcb9fafbb
EwokOzwok <eeozmat@buffalo.edu>
2023-11-19 12:39:24 -0500

    committed

--- app.R
+++ app.R
@@ -7,11 +7,11 @@ options("golem.app.prod" = TRUE,
          # whenever there is one account token found, use the cached token
          gargle_oauth_email = TRUE,
          # specify auth tokens should be stored in a hidden directory ".secrets"
-         gargle_oauth_cache = "./.secrets")
+         gargle_oauth_cache = "MHScreener/.secrets")
 
 MHScreener::run_app() # add parameters here (if any)
 
-#
+
 # googledrive::drive_auth()
 # googlesheets4::gs4_auth()
 # googlesheets4::gs4_create(name = "MHScreenerTest",
